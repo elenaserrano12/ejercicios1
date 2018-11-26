d3b06e14b334ecd23419675040d1bb86f351c1ed
elenaserrano12 <43232642+elenaserrano12@users.noreply.github.com>
2018-11-26 17:12:57 +0100

    Delete bucle_5.py

--- bucle_5.py
+++
@@ -1,9 +0,0 @@
-def bucle_5():
-    numero=input("desde que numero empezamos la cuenta atras: ")
-    #for i in range (numero,0.-1):
-    i=numero
-    while(i>0) :
-        print i
-        i=i-1
-
-bucle_5()
